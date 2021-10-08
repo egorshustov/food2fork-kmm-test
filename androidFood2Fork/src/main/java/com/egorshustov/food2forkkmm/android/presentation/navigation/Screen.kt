22362d557e2055f9ae9400cd77b2d8b2f330e7cc
Egor Shustov <shustov.egor@kupibilet.ru>
2021-10-08 23:33:28 +0300

    Screens defined for Navigation with Jetpack Compose

---
+++ androidFood2Fork/src/main/java/com/egorshustov/food2forkkmm/android/presentation/navigation/Screen.kt
@@ -0,0 +1,8 @@
+package com.egorshustov.food2forkkmm.android.presentation.navigation
+
+sealed class Screen(val route: String) {
+
+    object RecipeList: Screen("recipeList")
+
+    object RecipeDetail: Screen("recipeDetail")
+}
