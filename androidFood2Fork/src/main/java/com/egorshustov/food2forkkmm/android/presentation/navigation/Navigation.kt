47e11c6976201992514d33fe90a5194f58e1015f
Egor Shustov <shustov.egor@kupibilet.ru>
2021-10-10 01:50:05 +0300

    recipeId argument passed when navigating between two screens

--- androidFood2Fork/src/main/java/com/egorshustov/food2forkkmm/android/presentation/navigation/Navigation.kt
+++ androidFood2Fork/src/main/java/com/egorshustov/food2forkkmm/android/presentation/navigation/Navigation.kt
@@ -1,29 +1,34 @@
 package com.egorshustov.food2forkkmm.android.presentation.navigation
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Button
-import androidx.compose.material.Divider
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.navArgument
 import androidx.navigation.compose.rememberNavController
+import com.egorshustov.food2forkkmm.android.presentation.recipe_detail.RecipeDetailScreen
+import com.egorshustov.food2forkkmm.android.presentation.recipe_list.RecipeListScreen
+
+private const val RECIPE_ID_ARG = "recipeId"
 
 @Composable
 fun Navigation() {
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = Screen.RecipeList.route) {
-        composable(route = Screen.RecipeList.route) { navBackStackEntry ->
-            Column {
-                Text(text = "RecipeListScreen")
-                Divider()
-                Button(onClick = { navController.navigate(Screen.RecipeDetail.route) }) {
-                    Text(text = "Go RecipeDetail")
-                }
+        composable(
+            route = Screen.RecipeList.route
+        ) { navBackStackEntry ->
+            RecipeListScreen { recipeId ->
+                navController.navigate(Screen.RecipeDetail.route + "/$recipeId")
             }
         }
-        composable(route = Screen.RecipeDetail.route) { navBackStackEntry ->
-            Text(text = "RecipeDetailScreen")
+        composable(
+            route = Screen.RecipeDetail.route + "/{$RECIPE_ID_ARG}",
+            arguments = listOf(navArgument(RECIPE_ID_ARG) {
+                type = NavType.IntType
+            })
+        ) { navBackStackEntry ->
+            RecipeDetailScreen(recipeId = navBackStackEntry.arguments?.getInt(RECIPE_ID_ARG))
         }
     }
 }
