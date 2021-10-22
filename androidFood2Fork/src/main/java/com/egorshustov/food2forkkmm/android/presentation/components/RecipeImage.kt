a3e6a848116cbe848c74165ae52b7e58eaabf151
Egor Shustov <shustov.egor@kupibilet.ru>
2021-10-22 04:29:42 +0300

    RecipeImage refactored, different background colors are showing current image painter state

--- androidFood2Fork/src/main/java/com/egorshustov/food2forkkmm/android/presentation/components/RecipeImage.kt
+++ androidFood2Fork/src/main/java/com/egorshustov/food2forkkmm/android/presentation/components/RecipeImage.kt
@@ -1,9 +1,12 @@
 package com.egorshustov.food2forkkmm.android.presentation.components
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -12,44 +15,43 @@ import coil.annotation.ExperimentalCoilApi
 import coil.compose.ImagePainter
 import coil.compose.rememberImagePainter
 
-const val RECIPE_IMAGE_HEIGHT = 260
+const val RECIPE_IMAGE_BOX_HEIGHT = 260
 
 @OptIn(ExperimentalCoilApi::class)
 @Composable
 fun RecipeImage(url: String, contentDescription: String) {
     val painter = rememberImagePainter(data = url)
 
-    Box {
-        Image(
-            painter = painter,
-            contentDescription = contentDescription,
-            contentScale = ContentScale.Crop,
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(RECIPE_IMAGE_HEIGHT.dp)
-        )
-    }
-
-    when (painter.state) {
-        ImagePainter.State.Empty -> {
-        }
-
-        is ImagePainter.State.Loading -> {
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(RECIPE_IMAGE_HEIGHT.dp),
-            ) {
-                // empty for white background
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(RECIPE_IMAGE_BOX_HEIGHT.dp),
+    ) {
+        when (painter.state) {
+            is ImagePainter.State.Loading -> {
+                Box(
+                    modifier = Modifier
+                        .background(MaterialTheme.colors.secondaryVariant)
+                        .fillMaxSize()
+                )
             }
-        }
 
-        is ImagePainter.State.Success -> {
-            // do something on success
-        }
+            is ImagePainter.State.Success, ImagePainter.State.Empty -> {
+                Image(
+                    painter = painter,
+                    contentDescription = contentDescription,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
 
-        is ImagePainter.State.Error -> {
-            // show error or whatever
+            is ImagePainter.State.Error -> {
+                Box(
+                    modifier = Modifier
+                        .background(MaterialTheme.colors.error)
+                        .fillMaxSize()
+                )
+            }
         }
     }
 }
