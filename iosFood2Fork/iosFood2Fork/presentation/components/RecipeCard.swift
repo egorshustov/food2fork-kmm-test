d1c6195819c0b5da98d7c630104302bbc8bba179
Egor Shustov <shustov.egor@kupibilet.ru>
2021-12-12 23:46:30 +0300

    RecipeCard.swift styling added

--- iosFood2Fork/iosFood2Fork/presentation/components/RecipeCard.swift
+++ iosFood2Fork/iosFood2Fork/presentation/components/RecipeCard.swift
@@ -19,16 +19,36 @@ struct RecipeCard: View {
     }
     
     var body: some View {
-        WebImage(url: URL(string: recipe.featuredImage))
-            .resizable() // 1
-            .placeholder(Image(systemName: "photo"))
-            .placeholder {
-                Rectangle().foregroundColor(.white)
+        VStack {
+            WebImage(url: URL(string: recipe.featuredImage))
+                .resizable() // 1
+                .placeholder(Image(systemName: "photo"))
+                .placeholder {
+                    Rectangle().foregroundColor(.white)
+                }
+                .indicator(.activity)
+                .transition(.fade(duration: 0.5))
+                .scaledToFill() // 2
+                .frame(height: 250, alignment: .center) // 3
+                .clipped() // 4
+            
+            HStack(alignment: .lastTextBaseline) {
+                Text(recipe.title)
+                    .font(.body)
+                    .frame(alignment: .center)
+                
+                Spacer()
+                
+                Text(String(recipe.rating))
+                    .frame(alignment: .trailing)
             }
-            .indicator(.activity)
-            .transition(.fade(duration: 0.5))
-            .scaledToFill() // 2
-            .frame(height: 250, alignment: .center) // 3
-            .clipped() // 4
+            .padding(.top, 8)
+            .padding(.leading, 8)
+            .padding(.trailing, 8)
+            .padding(.bottom, 12)
+        }
+        .background(Color.black)
+        .cornerRadius(8)
+        .shadow(radius: 5)
     }
 }
